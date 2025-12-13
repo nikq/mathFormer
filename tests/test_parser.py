669b6fea7fdfbc9926231a6fb026ae7ee9a85cfe
nikq <nikutama@gmail.com>
2025-12-13 15:30:36 +0900

    add files

---
+++ tests/test_parser.py
@@ -0,0 +1,36 @@
+# test_parse.py
+from src.ast import parse, Node
+import unittest
+
+class TestNewOps(unittest.TestCase):
+    def test_parse(self):
+        node = parse("1 + 2")
+        self.assertEqual(node.left.value, 1)
+        self.assertEqual(node.right.value, 2)
+        self.assertEqual(node.op, '+')
+
+    def test_unary(self):
+        node = parse("next(1)")
+        self.assertEqual(node.child.value, 1)
+        self.assertEqual(node.op, 'next')
+
+    def test_binary(self):
+        node = parse("max(1, 2)")
+        self.assertEqual(node.left.value, 1)
+        self.assertEqual(node.right.value, 2)
+        self.assertEqual(node.op, 'max')
+
+    def test_depth2(self):
+        node = parse("max(1, next(2))")
+        self.assertEqual(node.left.value, 1)
+        self.assertEqual(node.right.child.value, 2)
+        self.assertEqual(node.op, 'max')
+
+    def test_depth3(self):
+        node = parse("-1--2")
+        self.assertEqual(node.left.child.value, 1)
+        self.assertEqual(node.right.child.value, 2)
+        self.assertEqual(node.op, '-')
+
+if __name__ == '__main__':
+    unittest.main()
